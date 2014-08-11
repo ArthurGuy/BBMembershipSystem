4f77db4d2884a4d3d33a4952fe04ca59467c8c0d
Arthur Guy <arthur@assys.net>
2014-08-11 22:27:29 +0100

    Updated the user image library to use an image library, also setup image croping and now generating two sizes of file.

--- app/BB/Helpers/UserImage.php
+++ app/BB/Helpers/UserImage.php
@@ -1,5 +1,7 @@
 <?php namespace BB\Helpers;
 
+use Intervention\Image\Facades\Image;
+
 class UserImage {
 
     protected static $bucket = 'buildbrighton-bbms-dev';
@@ -11,57 +13,52 @@ class UserImage {
 
     public function uploadPhoto($userId, $filePath)
     {
-        $fileContent = file_get_contents($filePath);
-        $fileInfo = getimagesize($filePath);
-        $fileInfo['mime'];
+        $tmpFilePath = storage_path("tmp")."/".$userId.".png";
+        $tmpFilePathThumb = storage_path("tmp")."/".$userId."-thumb.png";
 
-
-        if ($fileInfo['mime'] == 'image/png')
-        {
-            $im = imagecreatefrompng($filePath);
-
-            //The image needs to be true colour in order for it to render properly in the pdf
-            if (!imageistruecolor($im))
-            {
-                //Convert the image from palette colour to standard RGBA and save back to the file system
-                imagepalettetotruecolor($im);
-                imagepng($im, $filePath);
-            }
-        }
-        else
-        {
-            imagepng(imagecreatefromstring($fileContent), storage_path("tmp")."/".$userId.".png");
-            $fileContent = file_get_contents(storage_path("tmp")."/".$userId.".png");
-            /*
-             * @TODO:delete this temp file
-             */
-        }
-
-        /*
-         * @TODO:Crop the image into the correct ratio
-         */
-
-
+        //Generate the thumbnail and larger image
+        Image::make($filePath)->fit(500)->save($tmpFilePath);
+        Image::make($filePath)->fit(200)->save($tmpFilePathThumb);
 
         $s3 = \AWS::get('s3');
         try {
             $s3->putObject(array(
                 'Bucket'        => self::$bucket,
                 'Key'           => \App::environment().'/user-photo/'.md5($userId).'.png',
-                'Body'          => $fileContent,
+                'Body'          => file_get_contents($tmpFilePath),
                 'ACL'           => 'public-read',
                 'ContentType'   => 'image/png'
             ));
         } catch(Exception $e) {
             \Log::exception($e);
-
             throw new UserImageFailedException();
         }
+
+        try {
+            $s3->putObject(array(
+                'Bucket'        => self::$bucket,
+                'Key'           => \App::environment().'/user-photo/'.md5($userId).'-thumb.png',
+                'Body'          => file_get_contents($tmpFilePathThumb),
+                'ACL'           => 'public-read',
+                'ContentType'   => 'image/png'
+            ));
+        } catch(Exception $e) {
+            \Log::exception($e);
+            throw new UserImageFailedException();
+        }
+
+        \File::delete($tmpFilePath);
+        \File::delete($tmpFilePathThumb);
+    }
+
+    public static function imageUrl($userId)
+    {
+        return "https://s3-eu-west-1.amazonaws.com/".self::$bucket."/".\App::environment().'/user-photo/'.md5($userId).'.png';
     }
 
     public static function thumbnailUrl($userId)
     {
-        return "https://s3-eu-west-1.amazonaws.com/".self::$bucket."/".\App::environment().'/user-photo/'.md5($userId).'.png';
+        return "https://s3-eu-west-1.amazonaws.com/".self::$bucket."/".\App::environment().'/user-photo/'.md5($userId).'-thumb.png';
     }
 
 } 
