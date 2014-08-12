7bcbfc180c7bf0a056032cf73aa938045b45626c
Arthur Guy <arthur@assys.net>
2014-08-12 15:37:31 +0100

    Update membership based on the IPN

--- app/controllers/PaypalIPNController.php
+++ app/controllers/PaypalIPNController.php
@@ -18,6 +18,35 @@ class PaypalIPNController extends \BaseController {
             \Log::error("Invalid IPN");
         }
 
-        \Log::debug(json_encode($ipnMessage->getRawData()));
+        $ipnData = $ipnMessage->getRawData();
+
+        \Log::debug(json_encode($ipnData));
+
+        if ($ipnData['txn_type'] == 'subscr_payment')
+        {
+            if ($ipnData['payment_status'] != 'Completed')
+            {
+                \Log::error("Received different payment status for sub payment. ".$ipnData['payment_status']);
+                return;
+            }
+            $user = User::where('email', $ipnData['payer_email'])->first();
+            if (!$user)
+            {
+                \Log::error("IPN Received for unknown email ".$ipnData['payer_email']);
+                return;
+            }
+            Payment::create([
+                'reason' => 'subscription',
+                'source' => 'paypal',
+                'source_id' => $ipnData['txn_id'],
+                'user_id' => $user->id,
+                'amount' => $ipnData['mc_gross'],
+                'fee' => $ipnData['mc_fee'],
+                'amount_minus_fee' => ($ipnData['mc_gross'] - $ipnData['mc_fee']),
+                'status' => 'paid'
+            ]);
+            $date = new \Carbon\Carbon();
+            $user->extendMembership('paypal', $date->addMonth());
+        }
     }
 } 
