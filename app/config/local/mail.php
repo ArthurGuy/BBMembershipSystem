3a5ff171741f37fbc4c937f1f21b69e18394f1a2
Arthur Guy <arthur@assys.net>
2014-08-12 10:29:35 +0100

    Email config tweaked for production

--- app/config/local/mail.php
+++ app/config/local/mail.php
@@ -54,59 +54,9 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'noreply@bbms.buildbrighton.com', 'name' => 'BBMS'),
+	'from' => array('address' => 'bbms@sandbox3012.mailgun.org', 'name' => 'Build Brighton Member System'),
 
-	/*
-	|--------------------------------------------------------------------------
-	| E-Mail Encryption Protocol
-	|--------------------------------------------------------------------------
-	|
-	| Here you may specify the encryption protocol that should be used when
-	| the application send e-mail messages. A sensible default using the
-	| transport layer security protocol should provide great security.
-	|
-	*/
 
-	'encryption' => 'tls',
-
-	/*
-	|--------------------------------------------------------------------------
-	| SMTP Server Username
-	|--------------------------------------------------------------------------
-	|
-	| If your SMTP server requires a username for authentication, you should
-	| set it here. This will get used to authenticate with your server on
-	| connection. You may also set the "password" value below this one.
-	|
-	*/
-
-	'username' => $_ENV['MAILGUN_USERNAME'],
-
-	/*
-	|--------------------------------------------------------------------------
-	| SMTP Server Password
-	|--------------------------------------------------------------------------
-	|
-	| Here you may set the password required by your SMTP server to send out
-	| messages from your application. This will be given to the server on
-	| connection so that the application will be able to send messages.
-	|
-	*/
-
-	'password' => $_ENV['MAILGUN_PASSWORD'],
-
-	/*
-	|--------------------------------------------------------------------------
-	| Sendmail System Path
-	|--------------------------------------------------------------------------
-	|
-	| When using the "sendmail" driver to send e-mails, we will need to know
-	| the path to where Sendmail lives on this server. A default path has
-	| been provided here, which will work well on most of your systems.
-	|
-	*/
-
-	'sendmail' => '/usr/sbin/sendmail -bs',
 
 	/*
 	|--------------------------------------------------------------------------
