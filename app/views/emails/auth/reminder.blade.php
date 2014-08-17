73e870bda904181bc6c2579fc73d61c3f392b18d
Arthur Guy <arthur@assys.net>
2014-08-17 02:39:27 +0100

    Tweaked password reset email text

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -7,7 +7,8 @@
 		<h2>Reset your password</h2>
 
 		<div>
-			To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.<br/>
+			To reset your password please visit this url:<br />
+            {{ URL::to('password/reset', array($token)) }}.<br/>
 			This link will expire in {{ Config::get('auth.reminder.expire', 60) }} minutes.
 		</div>
 	</body>
