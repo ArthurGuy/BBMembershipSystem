0788d7ca71438dc00a0789d040cad6ca8d0ed1d2
Arthur Guy <arthur@assys.net>
2014-08-19 12:10:49 +0100

    PayPal IPN handling updated to only cancel payment if the user is still setup with paypal

--- app/controllers/PaypalIPNController.php
+++ app/controllers/PaypalIPNController.php
@@ -2,13 +2,14 @@
 
 use BB\Helpers\PayPalConfig;
 
-class PaypalIPNController extends \BaseController {
+class PaypalIPNController extends \BaseController
+{
 
     public function receiveNotification()
     {
         $ipnMessage = new \PayPal\IPN\PPIPNMessage('', PayPalConfig::getConfig());
 
-        if($ipnMessage->validate()) {
+        if ($ipnMessage->validate()) {
             //\Log::debug("IPN Success");
         } else {
             \Log::error("Invalid IPN");
@@ -18,42 +19,40 @@ class PaypalIPNController extends \BaseController {
 
         //\Log::debug(json_encode($ipnData));
 
-        if (isset($ipnData['txn_type']) && ($ipnData['txn_type'] == 'subscr_payment'))
-        {
-            if ($ipnData['payment_status'] != 'Completed')
-            {
-                \Log::error("Received different payment status for sub payment. ".$ipnData['payment_status']);
+        if (isset($ipnData['txn_type']) && ($ipnData['txn_type'] == 'subscr_payment')) {
+            if ($ipnData['payment_status'] != 'Completed') {
+                \Log::error("Received different payment status for sub payment. " . $ipnData['payment_status']);
                 return;
             }
             $user = User::where('email', $ipnData['payer_email'])->first();
-            if (!$user)
-            {
+            if (!$user) {
                 $user = User::where('secondary_email', $ipnData['payer_email'])->first();
             }
-            if (!$user)
-            {
-                \Log::error("IPN Received for unknown email ".$ipnData['payer_email']);
+            if (!$user) {
+                \Log::error("IPN Received for unknown email " . $ipnData['payer_email']);
                 return;
             }
-            Payment::create([
-                'reason' => 'subscription',
-                'source' => 'paypal',
-                'source_id' => $ipnData['txn_id'],
-                'user_id' => $user->id,
-                'amount' => $ipnData['mc_gross'],
-                'fee' => $ipnData['mc_fee'],
-                'amount_minus_fee' => ($ipnData['mc_gross'] - $ipnData['mc_fee']),
-                'status' => 'paid'
-            ]);
+            Payment::create(
+                [
+                    'reason'           => 'subscription',
+                    'source'           => 'paypal',
+                    'source_id'        => $ipnData['txn_id'],
+                    'user_id'          => $user->id,
+                    'amount'           => $ipnData['mc_gross'],
+                    'fee'              => $ipnData['mc_fee'],
+                    'amount_minus_fee' => ($ipnData['mc_gross'] - $ipnData['mc_fee']),
+                    'status'           => 'paid'
+                ]
+            );
             $date = new \Carbon\Carbon();
             $user->extendMembership('paypal', $date->addMonth());
-        }
-        elseif (isset($ipnData['txn_type']) && ($ipnData['txn_type'] == 'subscr_cancel'))
-        {
+        } elseif (isset($ipnData['txn_type']) && ($ipnData['txn_type'] == 'subscr_cancel')) {
             $user = User::where('email', $ipnData['payer_email'])->first();
-            if ($user)
-            {
-                $user->cancelSubscription();
+            if ($user) {
+                //The user may have already changed to another method, only cancel if its still paypal
+                if ($user->payment_method == 'paypal') {
+                    $user->cancelSubscription();
+                }
             }
         }
     }
--- app/views/emails/payment-warning.blade.php
+++ app/views/emails/payment-warning.blade.php
@@ -6,8 +6,11 @@
 <body>
 <p>
     Hi {{ $user->given_name }},<br />
-    We haven't been notified about your latest subscription payment so your account has been flagged with a payment warning.<br />
+    We haven't been notified about your latest subscription payment or you have cancelled your direct debit or PayPal
+    subscription. Your account has been flagged with a payment warning but if your in the process of setting up a new
+    subscription this will be cleared.<br />
     <br />
+    If your changing payment method please disregard this email.<br />
     If you have paid please email us asap with the details and we will see what went wrong.<br />
     If you have decided to leave it would be great if you could click the leaving button in your account or email us and let us know.
 </p>
