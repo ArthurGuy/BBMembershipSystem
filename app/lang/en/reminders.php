eca18e1e3757a8951c40c31efa59fe2329e227d5
Arthur Guy <arthur@assys.net>
2014-08-19 03:04:48 +0100

    Updated password reminder process validation text

--- app/lang/en/reminders.php
+++ app/lang/en/reminders.php
@@ -13,12 +13,12 @@ return array(
 	|
 	*/
 
-	"password" => "Passwords must be at least six characters and match the confirmation.",
+	"password" => "Passwords must be at least 8 characters.",
 
 	"user" => "We can't find a user with that e-mail address.",
 
 	"token" => "This password reset token is invalid.",
 
-	"sent" => "Password reminder sent!",
+	"sent" => "Password reminder sent",
 
 );
