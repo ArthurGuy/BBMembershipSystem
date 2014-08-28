07585b88d62b0173369df274968a0e871c8a8567
Arthur Guy <arthur@assys.net>
2014-08-28 20:05:30 +0200

    Test case updated to setup a sqlite database

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -2,6 +2,17 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
+    /**
+     * Default preparation for each test
+     *
+     */
+    public function setUp()
+    {
+        parent::setUp(); // Don't forget this!
+
+        $this->prepareForTests();
+    }
+
 	/**
 	 * Creates the application.
 	 *
@@ -15,5 +26,16 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
+
+    /**
+     * Migrates the database and set the mailer to 'pretend'.
+     * This will cause the tests to run quickly.
+     *
+     */
+    private function prepareForTests()
+    {
+        Artisan::call('migrate');
+        Mail::pretend(true);
+    }
 
 }
