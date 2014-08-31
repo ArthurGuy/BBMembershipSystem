c77bc3894dacbea040c0d9d515735ceb455729bc
Arthur Guy <arthur@assys.net>
2014-08-31 15:03:42 +0100

    notifications updated on the edit member form

--- app/views/account/edit.blade.php
+++ app/views/account/edit.blade.php
@@ -6,60 +6,60 @@
 
 <div class="row">
     <div class="col-xs-12 col-md-4">
-        <div class="form-group {{ $errors->has('given_name') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('given_name', 'has-error has-feedback') }}">
             {{ Form::label('given_name', 'First Name') }}
             {{ Form::text('given_name', null, ['class'=>'form-control', 'x-autocompletetype'=>'given-name']) }}
-            {{ $errors->first('given_name', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('given_name') }}
         </div>
     </div>
     <div class="col-xs-12 col-md-4">
-        <div class="form-group {{ $errors->has('family_name') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('family_name', 'has-error has-feedback') }}">
             {{ Form::label('family_name', 'Family Name') }}
             {{ Form::text('family_name', null, ['class'=>'form-control', 'x-autocompletetype'=>'surname']) }}
-            {{ $errors->first('family_name', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('family_name') }}
         </div>
     </div>
 </div>
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('email') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('email', 'has-error has-feedback') }}">
             {{ Form::label('email', 'Email') }}
             {{ Form::text('email', null, ['class'=>'form-control', 'x-autocompletetype'=>'email']) }}
-            {{ $errors->first('email', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('email') }}
         </div>
     </div>
 </div>
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('secondary_email') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('secondary_email', 'has-error has-feedback') }}">
             {{ Form::label('secondary_email', 'Alternate Email') }}
             {{ Form::text('secondary_email', null, ['class'=>'form-control']) }}
             <span class="help-block">If your paying through PayPal and that account has a different address or your using a different email with google groups please enter it here</span>
-            {{ $errors->first('secondary_email', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('secondary_email') }}
         </div>
     </div>
 </div>
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('password') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('password', 'has-error has-feedback') }}">
             {{ Form::label('password', 'Password') }}
             {{ Form::password('password', ['class'=>'form-control', 'autocomplete'=>'off']) }}
-            {{ $errors->first('password', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('password') }}
         </div>
     </div>
 </div>
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('monthly_subscription') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('monthly_subscription', 'has-error has-feedback') }}">
             {{ Form::label('monthly_subscription', 'Monthly Subscription Amount') }}
             <div class="input-group">
                 <div class="input-group-addon">&pound;</div>
                 {{ Form::text('monthly_subscription', null, ['class'=>'form-control', 'placeholder'=>'20']) }}
             </div>
-            {{ $errors->first('monthly_subscription', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('monthly_subscription') }}
             <span class="help-block">How much do you want to contribute each month, the average is around £20</span>
         </div>
     </div>
@@ -67,30 +67,30 @@
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('address_line_1') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('address_line_1', 'has-error has-feedback') }}">
             {{ Form::label('address_line_1', 'Address Line 1') }}
             {{ Form::text('address_line_1', null, ['class'=>'form-control', 'x-autocompletetype'=>'address-line-1']) }}
-            {{ $errors->first('address_line_1', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('address_line_1') }}
         </div>
     </div>
 </div>
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('address_line_2') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('address_line_2', 'has-error has-feedback') }}">
             {{ Form::label('address_line_2', 'Address Line 2') }}
             {{ Form::text('address_line_2', null, ['class'=>'form-control', 'x-autocompletetype'=>'address-line-2']) }}
-            {{ $errors->first('address_line_2', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('address_line_2') }}
         </div>
     </div>
 </div>
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('address_line_3') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('address_line_3', 'has-error has-feedback') }}">
             {{ Form::label('address_line_3', 'Address Line 3') }}
             {{ Form::text('address_line_3', null, ['class'=>'form-control', 'x-autocompletetype'=>'address-locality']) }}
-            {{ $errors->first('address_line_3', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('address_line_3') }}
         </div>
     </div>
 </div>
@@ -98,30 +98,30 @@
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('address_line_4') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('address_line_4', 'has-error has-feedback') }}">
             {{ Form::label('address_line_4', 'Address Line 4') }}
             {{ Form::text('address_line_4', null, ['class'=>'form-control', 'x-autocompletetype'=>'region']) }}
-            {{ $errors->first('address_line_4', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('address_line_4') }}
         </div>
     </div>
 </div>
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('address_postcode') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('address_postcode', 'has-error has-feedback') }}">
             {{ Form::label('address_postcode', 'Post Code') }}
             {{ Form::text('address_postcode', null, ['class'=>'form-control', 'x-autocompletetype'=>'postal-code']) }}
-            {{ $errors->first('address_postcode', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('address_postcode') }}
         </div>
     </div>
 </div>
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('emergency_contact') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('emergency_contact', 'has-error has-feedback') }}">
             {{ Form::label('emergency_contact', 'Emergency Contact') }}
             {{ Form::text('emergency_contact', null, ['class'=>'form-control']) }}
-            {{ $errors->first('emergency_contact', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('emergency_contact') }}
             <span class="help-block">Please give us the name and contact details of someone we can contact if needed</span>
         </div>
     </div>
@@ -137,10 +137,10 @@
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('profile_photo') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('profile_photo', 'has-error has-feedback') }}">
             {{ Form::label('profile_photo', 'Profile Photo', ['class'=>'control-label']) }}
             {{ Form::file('profile_photo', null, ['class'=>'form-control']) }}
-            {{ $errors->first('profile_photo', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('profile_photo') }}
             <span class="help-block">This must be a clear image of your face, its not much use for identification otherwise!</span>
             <span class="help-block">This photo will be displayed to members and may be used within the space, it will also be listed publicly on this site but you can turn that off below if you want.</span>
         </div>
@@ -155,10 +155,10 @@
 
 <div class="row">
     <div class="col-xs-12 col-md-8">
-        <div class="form-group {{ $errors->has('profile_photo_private') ? 'has-error has-feedback' : '' }}">
+        <div class="form-group {{ Notification::hasErrorDetail('profile_photo_private', 'has-error has-feedback') }}">
             {{ Form::checkbox('profile_photo_private', true, null, ['class'=>'']) }}
             {{ Form::label('profile_photo_private', 'Make my photo private', ['class'=>'']) }}
-            {{ $errors->first('profile_photo_private', '<span class="help-block">:message</span>') }}
+            {{ Notification::getErrorDetail('profile_photo_private') }}
             <span class="help-block">If you want to block your photo from displaying outside Build Brighton please check this box although we would rather you didn't.</span>
         </div>
     </div>
