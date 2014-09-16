fe0ec768d44ad63b5c6cd01271d1e5a9e2ad2f5a
Arthur Guy <arthur@assys.net>
2014-09-16 11:50:41 +0100

    Added a tooltip for the sub expiry date for standing order users #21

--- app/views/account/partials/member-status-bar.blade.php
+++ app/views/account/partials/member-status-bar.blade.php
@@ -39,7 +39,14 @@
 
     @if ($user->subscription_expires)
     <li>
-        <p class="navbar-text">Subscription Expires: {{ $user->present()->subscriptionExpiryDate }}</p>
+        <p class="navbar-text">
+            Subscription Expires:
+            @if ($user->payment_method == 'standing-order')
+                <span data-toggle="tooltip" data-placement="top" title="Confirmation of subscription payments happen once a month so this date may not be up to date">{{ $user->present()->subscriptionExpiryDate }}</span>
+            @else
+                {{ $user->present()->subscriptionExpiryDate }}
+            @endif
+        </p>
     </li>
     @endif
 </ul>
