91ecbee624249430325e3186da7f11eab3c86e8c
Arthur Guy <arthur@assys.net>
2014-09-16 15:42:20 +0100

    Updated the access log observer to fetch the users avatar url and name and send that via pusher

--- app/BB/Observer/AccessLogObserver.php
+++ app/BB/Observer/AccessLogObserver.php
@@ -1,6 +1,7 @@
 <?php namespace BB\Observer;
 
 use Artdarek\Pusherer\Facades\Pusherer;
+use BB\Helpers\UserImage;
 use Illuminate\Support\Facades\Log;
 
 class AccessLogObserver
@@ -14,13 +15,25 @@ class AccessLogObserver
     public function saved($accessLog)
     {
         try {
+            $userName     = null;
+            $userImageUrl = UserImage::anonymous();
+            $user         = \User::find($accessLog->user_id);
+            if ($user) {
+                $userName = $user->name;
+                if ($user->profile_photo) {
+                    $userImageUrl = UserImage::imageUrl($user->hash);
+                }
+            }
+
             Pusherer::trigger(
                 'activity',
                 $accessLog->service,
                 array(
                     'user_id'    => $accessLog->user_id,
                     'response'   => $accessLog->response,
-                    'key_fob_id' => $accessLog->key_fob_id
+                    'key_fob_id' => $accessLog->key_fob_id,
+                    'user_name'  => $userName,
+                    'user_image' => $userImageUrl
                 )
             );
         } catch (\Exception $e) {
