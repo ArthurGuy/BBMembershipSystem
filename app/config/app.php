8623b478ad946a92ed8a6dab5837b57b19dd3ee4
Arthur Guy <arthur@assys.net>
2014-09-17 12:05:12 +0100

    Updated the default site url - this is used on cli tasks, specifically the welcome email verify link

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://localhost',
+	'url' => 'https://bbms.buildbrighton.com',
 
 	/*
 	|--------------------------------------------------------------------------
