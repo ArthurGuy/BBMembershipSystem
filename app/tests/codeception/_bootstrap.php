1a0a6696d0123e21e007979f6a2ae89d773426e9
Arthur Guy <arthur@assys.net>
2014-09-18 16:55:44 +0100

    tidied codeception bootstrap

--- app/tests/codeception/_bootstrap.php
+++ app/tests/codeception/_bootstrap.php
@@ -2,8 +2,6 @@
 // This is global bootstrap for autoloading
 putenv('APP_ENV=testing');
 
-//\App::call('php artisan migrate --env=testing --database=testing');
-
-
+//Delete the test db and replace it with a known quantity
 exec('rm ' . __DIR__ . '/../../database/bbms_testing.sqlite');
 exec('cp ' . __DIR__ . '/../../database/bbms_testing_clean.sqlite ' . __DIR__ .'/../../database/bbms_testing.sqlite');
