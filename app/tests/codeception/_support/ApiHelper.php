face844329d3cffa0b1a73558bd8c237d1681de9
Arthur Guy <arthur@assys.net>
2014-09-18 16:56:20 +0100

    New codeception api test suite Two new tests for the legacy endpoint

---
+++ app/tests/codeception/_support/ApiHelper.php
@@ -0,0 +1,21 @@
+<?php
+namespace Codeception\Module;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class ApiHelper extends \Codeception\Module
+{
+
+    public function getActiveKeyholderMember()
+    {
+        //We know this is user 1 in the DB
+        return \User::findOrFail(1);
+    }
+
+
+    public function getMemberKeyFob($userId)
+    {
+        return \KeyFob::where('user_id', $userId)->first();
+    }
+}
