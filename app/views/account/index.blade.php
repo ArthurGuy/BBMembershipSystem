be6312ae1bb4a190b344f6998a06708b031eb4ee
Arthur Guy <arthur@assys.net>
2014-09-25 15:38:18 +0100

    Refactored the admin user view

--- app/views/account/index.blade.php
+++ app/views/account/index.blade.php
@@ -27,56 +27,8 @@
             <th>Payment</th>
         </tr>
     </thead>
-@foreach ($users as $user)
     <tbody>
-        <tr>
-            <td>
-                @if ($user->profile_photo)
-                <img src="{{ \BB\Helpers\UserImage::thumbnailUrl($user->hash) }}" width="100" height="100" class="img-circle" />
-                @else
-                <img src="{{ \BB\Helpers\UserImage::anonymous() }}" width="100" height="100" class="img-circle" />
-                @endif
-            </td>
-            <td>
-                <a href="{{ route('account.show', $user->id) }}">{{ $user->name }}</a>
-                @if ($user->hasRole('admin'))
-                <span class="label label-danger">Admin</span>
-                @endif
-            </td>
-            <td>{{ $user->email }}</td>
-            <td>
-                @if ($user->active)
-                <span class="glyphicon glyphicon-ok"></span>
-                @else
-                <span class="glyphicon glyphicon-remove"></span>
-                @endif
-            </td>
-            <td>{{ HTML::statusLabel($user->status) }}</td>
-            <td>
-                @if($user->key_holder)
-                    Yes
-                @else
-                    No
-                @endif
-            </td>
-            <td>
-                @if ($user->induction_completed)
-                <span class="glyphicon glyphicon-ok"></span>
-                @else
-                <span class="glyphicon glyphicon-remove"></span>
-                @endif
-            </td>
-            <td>{{ $user->present()->paymentMethod }}</td>
-            <td>{{ $user->present()->subscriptionExpiryDate }}</td>
-            <td>
-                {{ Form::open(array('method'=>'POST', 'class'=>'well form-inline', 'route' => ['account.payment.store', $user->id])) }}
-                {{ Form::hidden('reason', 'subscription') }}
-                {{ Form::select('source', ['other'=>'Other', 'paypal'=>'PayPal', 'cash'=>'Cash'], null, ['class'=>'form-control']) }}
-                {{ Form::submit('Record A &pound;'.round($user->monthly_subscription).' Payment', array('class'=>'btn btn-default')) }}
-                {{ Form::close() }}
-            </td>
-        </tr>
+        @each('account.index-row', $users, 'user')
     </tbody>
-@endforeach
 </table>
 <?php echo $users->links(); ?>
