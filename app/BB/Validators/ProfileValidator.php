5e2e77331d44df9913823092f76647cc3d9ecc2b
Arthur Guy <arthur@assys.net>
2014-09-25 13:55:36 +0100

    Profile Validator #28

---
+++ app/BB/Validators/ProfileValidator.php
@@ -0,0 +1,18 @@
+<?php namespace BB\Validators;
+
+class ProfileValidator extends FormValidator
+{
+
+    protected $rules = [
+        'twitter'      => 'max:50',
+        'facebook'     => 'max:50',
+        'google_plus'  => 'max:50',
+        'github'       => 'max:50',
+        'irc'          => 'max:50',
+        'website'      => 'url|max:100',
+        'tagline'      => 'max:250',
+        'description'  => '',
+        'skills_array' => '',
+    ];
+
+} 
