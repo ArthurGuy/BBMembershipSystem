3bb7bfefe3a2fe6f526387b5af479fa211b7b6b5
Arthur Guy <arthur@assys.net>
2014-09-27 13:31:24 +0100

    Updating of the profile data

--- app/views/account/profile/edit.blade.php
+++ app/views/account/profile/edit.blade.php
@@ -3,10 +3,10 @@
 
 <div class="page-header">
     <h1>Fill in your profile</h1>
-    <p>Enter as much or as little you want to share with others</p>
+    <p>This information will be shared with others, enter as much or as little as you want</p>
 </div>
 
-{{ Form::model($profileData, array('route' => ['account.update', $userId], 'method'=>'PUT', 'files'=>true)) }}
+{{ Form::model($profileData, array('route' => ['account.profile.update', $userId], 'method'=>'PUT', 'files'=>true)) }}
 
 <div class="row">
     <div class="col-xs-12 col-md-6">
@@ -39,7 +39,7 @@
         <div class="form-group {{ Notification::hasErrorDetail('google_plus', 'has-error has-feedback') }}">
             {{ Form::label('google_plus', 'Google+') }}
             <div class="input-group">
-                <div class="input-group-addon">https://plus.google.com/</div>
+                <div class="input-group-addon">https://plus.google.com/+</div>
                 {{ Form::text('google_plus', null, ['class'=>'form-control']) }}
             </div>
             {{ Notification::getErrorDetail('google_plus') }}
@@ -62,6 +62,16 @@
 
 <div class="row">
     <div class="col-xs-12 col-md-6">
+        <div class="form-group {{ Notification::hasErrorDetail('irc', 'has-error has-feedback') }}">
+            {{ Form::label('irc', 'IRC') }}
+            {{ Form::text('irc', null, ['class'=>'form-control']) }}
+            {{ Notification::getErrorDetail('irc') }}
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-md-6">
         <div class="form-group {{ Notification::hasErrorDetail('website', 'has-error has-feedback') }}">
             {{ Form::label('website', 'Website') }}
             {{ Form::text('website', null, ['class'=>'form-control']) }}
@@ -72,10 +82,30 @@
 
 <div class="row">
     <div class="col-xs-12 col-md-6">
-        <div class="form-group {{ Notification::hasErrorDetail('website', 'has-error has-feedback') }}">
-            {{ Form::label('tagline', 'Website') }}
-            {{ Form::text('website', null, ['class'=>'form-control']) }}
-            {{ Notification::getErrorDetail('website') }}
+        <div class="form-group {{ Notification::hasErrorDetail('tagline', 'has-error has-feedback') }}">
+            {{ Form::label('tagline', 'Tagline') }}
+            {{ Form::text('tagline', null, ['class'=>'form-control']) }}
+            {{ Notification::getErrorDetail('tagline') }}
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-md-6">
+        <div class="form-group {{ Notification::hasErrorDetail('description', 'has-error has-feedback') }}">
+            {{ Form::label('description', 'Description') }}
+            {{ Form::textarea('description', null, ['class'=>'form-control']) }}
+            {{ Notification::getErrorDetail('description') }}
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-md-6">
+        <div class="form-group {{ Notification::hasErrorDetail('skills', 'has-error has-feedback') }}">
+            {{ Form::label('skills', 'Skills') }}
+            {{ Form::select('skills[]', $skills, null, ['class'=>'form-control', 'multiple']) }}
+            {{ Notification::getErrorDetail('skills') }}
         </div>
     </div>
 </div>
