df8d5afecbb188e54a99f71cab8c8bb3a9a7597d
Arthur Guy <arthur@assys.net>
2014-10-13 21:51:49 +0100

    Validators updated #33

--- app/BB/Validators/ProfileValidator.php
+++ app/BB/Validators/ProfileValidator.php
@@ -4,15 +4,27 @@ class ProfileValidator extends FormValidator
 {
 
     protected $rules = [
-        'twitter'     => 'max:50',
-        'facebook'    => 'max:50',
-        'google_plus' => 'max:50',
-        'github'      => 'max:50',
-        'irc'         => 'max:50',
-        'website'     => 'url|max:100',
-        'tagline'     => 'max:250',
-        'description' => '',
-        'skills'      => 'array',
+        'twitter'               => 'max:50',
+        'facebook'              => 'max:50',
+        'google_plus'           => 'max:50',
+        'github'                => 'max:50',
+        'irc'                   => 'max:50',
+        'website'               => 'url|max:100',
+        'tagline'               => 'max:250',
+        'description'           => '',
+        'skills'                => 'array',
+        'profile_photo'         => 'required|image',
+        'profile_photo_private' => 'boolean',
+    ];
+
+    //During an update these rules will override the ones above
+    protected $updateRules = [
+        'profile_photo' => 'image',
+    ];
+
+
+    protected $adminOverride = [
+        'profile_photo' => 'image',
     ];
 
 } 
