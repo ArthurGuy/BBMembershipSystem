212ec448a40fb70307114f46020c34b2f18e17ff
Arthur Guy <arthur@assys.net>
2014-10-21 22:10:45 +0100

    New feedback link and modal along with a controller for managing it

---
+++ app/views/emails/feedback.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<p>
+    {{ $memberName }} has left feedback about the website
+</p>
+<p>
+    {{ nl2br($comments) }}
+</p>
+
+</body>
+</html>
--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -165,6 +165,38 @@
     </footer>
 
 
+    <div class="feedbackWidgetButtonWrap">
+        <div id="feedbackWidgetButton" data-toggle="modal" data-target="#feedbackWidgetModal">
+            <span class="glyphicon glyphicon-bullhorn"></span> Feedback
+        </div>
+    </div>
+    <div class="modal fade" id="feedbackWidgetModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Member System Feedback</h4>
+                </div>
+                {{ Form::open(array('method'=>'POST', 'route' => ['feedback.store'], 'class'=>'js-feedbackModalForm js-ajaxForm', 'data-successFunction'=>'feedbackFormSuccess', 'data-errorFunction'=>'feedbackFormError')) }}
+                <div class="modal-body">
+                    <p>If you have any comments or thoughts or ideas about the operation of the BBMS system we would love to hear them</p>
+
+                    <div class="form-group js-field-comments">
+                        {{ Form::label('comments', 'Your Thoughts') }}
+                        {{ Form::textarea('comments', null, ['class'=>'form-control', ''=>'']) }}
+                        <span class="help-block js-errorMessages"></span>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    {{ Form::button('Close', ['class'=>'btn btn-default', 'data-dismiss'=>'modal']) }}
+                    {{ Form::submit('Submit', ['class'=>'btn btn-primary']) }}
+                </div>
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
+
     <!-- Inline for now - this will be fixed at some stage -->
     <script>
     $('.js-ajaxForm').on('submit', function(event) {
@@ -195,6 +227,23 @@
             dataType: 'json'
         });
     });
+    function feedbackFormSuccess(data) {
+        if (data.success) {
+            $('#feedbackWidgetModal').modal('hide');
+            $('.js-feedbackModalForm textarea').val('');
+        }
+    }
+    function feedbackFormError(xhr, status, error) {
+        var errors = eval("(" + xhr.responseText + ")");
+        for(var n in errors) {
+            var $field = $('.js-feedbackModalForm').find('.js-field-'+n+'');
+            $field.addClass('has-error');
+            $field.find('.js-errorMessages').text(errors[n]);
+        }
+    }
+    </script>
+
+
     @include('partials/page-js')
 
     <script src="/js/lib/bootstrap.min.js"></script>
@@ -211,6 +260,8 @@
     </script>
 
     @yield('footer-js')
+
+
 
 </body>
 </html>
