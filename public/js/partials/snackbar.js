f30499f7a24d8516cb87e38b3a27db641b2fe4b1
Arthur Guy <arthur@assys.net>
2014-10-31 09:04:55 +0000

    JS for the new snackbar

--- public/js/partials/snackbar.js
+++ public/js/partials/snackbar.js
@@ -1,2 +1,60 @@
+BB.SnackBar = {};
 
-var foo = 'bar';
+BB.SnackBar = {
+
+    onScreen: false,
+    elementCreated: false,
+    snackBarDiv: false,
+    timeoutID: null,
+
+    displayMessage: function(message, details) {
+
+        if (!this.elementCreated) {
+            this.createElement();
+        }
+
+        if (this.onScreen) {
+            //remove the old one
+            this.remove();
+        }
+
+        //Add the content to the div
+        var snackBarContent = document.createTextNode(message);
+        this.snackBarDiv.innerHTML = '';
+        this.snackBarDiv.appendChild(snackBarContent);
+
+        this.display();
+
+        var self = this;
+        this.timeoutID = setTimeout(function () {
+            self.remove();
+        }, 3000);
+    },
+    display: function() {
+        //Remove the hidden class
+        this.snackBarDiv.className = 'snackBar';
+
+        this.onScreen = true;
+    },
+    remove: function() {
+        window.clearTimeout(this.timeoutID);
+
+        this.snackBarDiv.className = this.snackBarDiv.className + ' snackBarHidden';
+
+        this.onScreen = false;
+    },
+    createElement: function() {
+        //Create the snackbar div
+        this.snackBarDiv = document.createElement("div");
+        this.snackBarDiv.className = 'snackBar snackBarHidden';
+
+        //Add the div to the page
+        document.body.appendChild(this.snackBarDiv);
+
+        this.elementCreated = true;
+    },
+    boot: function() {
+        this.createElement();
+    }
+}
+BB.SnackBar.boot();
