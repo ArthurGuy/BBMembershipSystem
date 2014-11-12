1b311d6168712d010cdba088b3ccfeda8ece6761
Arthur Guy <arthur@assys.net>
2014-11-12 22:10:45 +0000

    Updated the style and wording of the cancel panel
    fixes #54

--- app/views/account/partials/cancel-panel.blade.php
+++ app/views/account/partials/cancel-panel.blade.php
@@ -1,18 +1,22 @@
-<div class="panel panel-danger">
+<div class="panel panel-default">
     <div class="panel-heading">
-        <h3 class="panel-title">Cancel</h3>
+        <h3 class="panel-title">Thinking of leaving?</h3>
     </div>
     <div class="panel-body">
+        <p>
+        If your thinking of leaving we would love to hear from you, please send us a message and we will see if we can help.<br />
+        <a href="mailto:trustees@buildbrighton.com">Email the trustees</a>
+        </p>
         @if ($user->payment_method == 'gocardless')
 
         {{ Form::open(array('method'=>'DELETE', 'route' => ['account.subscription.destroy', $user->id, 1])) }}
-        {{ Form::submit('Cancel Your Monthly Direct Debit', array('class'=>'btn btn-danger')) }}
+        {{ Form::submit('Cancel Your Monthly Direct Debit', array('class'=>'btn btn-link')) }}
         {{ Form::close() }}
 
         @else
 
         {{ Form::open(array('method'=>'DELETE', 'route' => ['account.destroy', $user->id])) }}
-        {{ Form::submit('Leave Build Brighton :(', array('class'=>'btn btn-danger')) }}
+        {{ Form::submit('Leave Build Brighton :(', array('class'=>'btn btn-link')) }}
         {{ Form::close() }}
 
         @endif
