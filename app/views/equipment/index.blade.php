0c85063d7c9887e4d26fe9cda8da314e74e147f5
Arthur Guy <arthur@assys.net>
2014-11-12 13:45:11 +0000

    refactored the equipment profile photos to use the new photo helper

--- app/views/equipment/index.blade.php
+++ app/views/equipment/index.blade.php
@@ -46,12 +46,8 @@ Tools and Equipment
                         <div class="list-group">
                             @foreach($trainers[$toolId] as $trainer)
                                 <a href="{{ route('members.show', $trainer->id) }}" class="list-group-item">
-                                        @if ($trainer->profile->profile_photo)
-                                            <img src="{{ \BB\Helpers\UserImage::thumbnailUrl($trainer->hash) }}" width="50" height="50" />
-                                        @else
-                                            <img src="{{ \BB\Helpers\UserImage::anonymous() }}" width="50" height="50" />
-                                        @endif
-                                        {{ $trainer->name }}
+                                    {{ HTML::memberPhoto($trainer->profile, $trainer->hash, 50, '') }}
+                                    {{ $trainer->name }}
                                 </a>
                             @endforeach
                         </div>
@@ -63,11 +59,7 @@ Tools and Equipment
                         <div class="list-group">
                         @foreach($usersPendingInduction[$toolId] as $user)
                             <a href="{{ route('members.show', $user->id) }}" class="list-group-item">
-                                @if ($user->profile->profile_photo)
-                                    <img src="{{ \BB\Helpers\UserImage::thumbnailUrl($user->hash) }}" width="25" height="25" />
-                                @else
-                                    <img src="{{ \BB\Helpers\UserImage::anonymous() }}" width="25" height="25" />
-                                @endif
+                                {{ HTML::memberPhoto($user->profile, $user->hash, 25, '') }}
                                 {{ $user->name }}
                             </a>
                         @endforeach
@@ -80,11 +72,7 @@ Tools and Equipment
                         <div class="list-group">
                         @foreach($trainedUsers[$toolId] as $user)
                             <a href="{{ route('members.show', $user->id) }}" class="list-group-item">
-                            @if ($user->profile->profile_photo)
-                                <img src="{{ \BB\Helpers\UserImage::thumbnailUrl($user->hash) }}" width="25" height="25" />
-                            @else
-                                <img src="{{ \BB\Helpers\UserImage::anonymous() }}" width="25" height="25" />
-                            @endif
+                            {{ HTML::memberPhoto($user->profile, $user->hash, 25, '') }}
                             {{ $user->name }}
                             </a>
                         @endforeach
