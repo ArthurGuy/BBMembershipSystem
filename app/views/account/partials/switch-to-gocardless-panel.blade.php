f992762ab008c62a1daf11fad63418c14b804689
Arthur Guy <arthur@assys.net>
2014-11-13 13:54:29 +0000

    Updated the text on the gocardless switch panel

--- app/views/account/partials/switch-to-gocardless-panel.blade.php
+++ app/views/account/partials/switch-to-gocardless-panel.blade.php
@@ -1,6 +1,6 @@
 <div class="panel panel-success">
     <div class="panel-heading">
-        <h3 class="panel-title">Switch to a Direct Debit</h3>
+        <h3 class="panel-title">Your Payment Method</h3>
     </div>
     <div class="panel-body">
         <div class="row">
@@ -9,8 +9,9 @@
                     We would be really grateful if you could switch to a Direct Debit payment
                 </p>
                 <div>
-                    Dealing with bank transfers takes up valuable time and PayPal charges us huge fees while Direct Debit payments are quick and fully automated.<br />
-                    Switching only takes a minute, just follow the link below to the <a href="https://gocardless.com/security" target="_blank">GoCardless</a> website (our payment processor) and complete the simple form.<br />
+                    Dealing with bank transfers takes time each month and introduces delays while PayPal charges us large fees.<br />
+                    A monthly Direct Debit payment is quick and fully automated and if you need you have the ability to cancel the payment from here or from you bank.<br />
+                    Switching only takes a minute, just follow the link below to the <a href="https://gocardless.com/security" target="_blank">GoCardless</a> website (our payment processor) and complete the form.<br />
                     <br />
                     <a href="{{ route('account.subscription.create', $user->id) }}" class="btn btn-primary">Setup a Direct Debit for &pound;{{ round($user->monthly_subscription) }}</a>
                     <small><a href="#" class="js-show-alter-subscription-amount">Change your monthly amount</a></small>
@@ -23,8 +24,6 @@
                     {{ Form::close() }}
                     <br />
                     <p>
-                    The direct debit will start when your current subscription expires.<br />
-                    You can cancel it at any point through this website, the GoCardless website or your bank giving you full control over the payments.<br />
                     <small>By switching you will also protected by the <a href="https://gocardless.com/direct-debit/guarantee/">direct debit guarantee.</a></small>
                     </p>
                 </div>
