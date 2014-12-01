b1e534b3b21d640b140fc02831f106d92c4550d2
Arthur Guy <arthur@assys.net>
2014-12-01 08:30:40 +0000

    Payment handle script updated to handle stripe js failure

--- public/js/partials/payments.js
+++ public/js/partials/payments.js
@@ -1,16 +1,20 @@
-var handler = StripeCheckout.configure({
-    key: stripePublicKey,
-    name: 'Build Brighton',
-    currency: 'GBP',
-    email: memberEmail,
-    allowRememberMe: false,
-    token: function(token) {
-        //Fill in the token and submit the form again
-        $('.js-stripeToken').val(token.id);
-        $('.js-multiPaymentForm').submit();
-    }
-});
-
+if (typeof StripeCheckout !== 'undefined') {
+    var stripeHandler = StripeCheckout.configure({
+        key: stripePublicKey,
+        name: 'Build Brighton',
+        currency: 'GBP',
+        email: memberEmail,
+        allowRememberMe: false,
+        token: function (token) {
+            //Fill in the token and submit the form again
+            $('.js-stripeToken').val(token.id);
+            $('.js-multiPaymentForm').submit();
+        }
+    });
+    var stripeAvailable = true;
+} else {
+    var stripeAvailable = false;
+}
 
 
 var multiPaymentFormChecked = false;
@@ -26,22 +30,27 @@ $('.js-multiPaymentForm').on('submit', function(event) {
 
     //Validation rules
     if (source == 'stripe') {
-        if ($('.js-stripeToken').val() == '') {
-            //Stripe is handled separately so stop this form post
+        if (!stripeAvailable) {
+            $(this).find('.help-block').text("Sorry, card processing is currently unavailable");
             event.preventDefault();
-            if (($(this).find('.js-amount').val() * 1) < 10) {
-                $(this).find('.help-block').text("Because of processing fees the payment must be £10 or over when paying by card");
-            } else {
-                var topUpAmount = ($(this).find('.js-amount').val() * 100);
-                var description = ($(this).find('.js-paymentDescription').val());
-
-                handler.open({
-                    description: description,
-                    amount: topUpAmount
-                });
-            }
         } else {
-            //Process is complete send it to the server
+            if ($('.js-stripeToken').val() == '') {
+                //Stripe is handled separately so stop this form post
+                event.preventDefault();
+                if (($(this).find('.js-amount').val() * 1) < 10) {
+                    $(this).find('.help-block').text("Because of processing fees the payment must be £10 or over when paying by card");
+                } else {
+                    var topUpAmount = ($(this).find('.js-amount').val() * 100);
+                    var description = ($(this).find('.js-paymentDescription').val());
+
+                    stripeHandler.open({
+                        description: description,
+                        amount: topUpAmount
+                    });
+                }
+            } else {
+                //Card collection is complete send it to the server
+            }
         }
     } else {
         //$(this).submit();
