53ab320d51b345a5398bb9e02fcf16573e5a5d5c
Arthur Guy <arthur@assys.net>
2014-12-13 15:39:26 +0000

    new relationship method

--- app/models/Role.php
+++ app/models/Role.php
@@ -9,4 +9,9 @@ class Role extends Eloquent {
      */
     protected $fillable = ['name'];
 
+    public function users()
+    {
+        return $this->belongsToMany('User')->withTimestamps();
+    }
+
 }
