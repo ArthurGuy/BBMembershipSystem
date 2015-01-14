fa5081f506ba947f885096512bbaeb12b9664640
Arthur Guy <arthur@assys.net>
2015-01-14 21:39:05 +0000

    added units back to the box sizing

--- app/views/storage_boxes/index.blade.php
+++ app/views/storage_boxes/index.blade.php
@@ -82,7 +82,7 @@
     <tbody>
         <tr @if($box->user && !$box->user->active)class="warning"@elseif(!$box->user)class="success"@endif>
             <td>{{ $box->id }}</td>
-            <td>{{ $box->size }}</td>
+            <td>{{ $box->size }}L</td>
             <td>{{ $box->user->name or 'Available' }}</td>
             <td>
                 @if($box->user && !$box->user->active)
