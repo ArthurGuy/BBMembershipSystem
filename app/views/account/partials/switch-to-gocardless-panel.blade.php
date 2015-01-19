11671b54f3b09a4500c8b8c26d14df572723e00e
Arthur Guy <arthur@assys.net>
2015-01-19 13:55:25 +0000

    updated to the dd switch panel to better communicate the switching process

--- app/views/account/partials/switch-to-gocardless-panel.blade.php
+++ app/views/account/partials/switch-to-gocardless-panel.blade.php
@@ -14,7 +14,7 @@
                     Switching only takes a minute, just follow the link below to the <a href="https://gocardless.com/security" target="_blank">GoCardless</a> website (our payment processor) and complete the form.<br />
                     <br />
                     <a href="{{ route('account.subscription.create', $user->id) }}" class="btn btn-primary">Setup a Direct Debit for &pound;{{ round($user->monthly_subscription) }}</a>
-                    <small><a href="#" class="js-show-alter-subscription-amount">Change your monthly amount</a></small>
+                    <small><a href="#" class="js-show-alter-subscription-amount">Change your monthly direct debit amount</a></small>
                     {{ Form::open(array('method'=>'POST', 'class'=>'form-inline hidden js-alter-subscription-amount-form', 'style'=>'display:inline-block', 'route' => ['account.update-sub-payment', $user->id])) }}
                     <div class="input-group">
                         <div class="input-group-addon">&pound;</div>
@@ -24,7 +24,8 @@
                     {{ Form::close() }}
                     <br />
                     <p>
-                    <small>By switching you will also protected by the <a href="https://gocardless.com/direct-debit/guarantee/">direct debit guarantee.</a></small>
+                        Don't forget to cancel your current subscription payment.<br />
+                        <small>By switching you will also protected by the <a href="https://gocardless.com/direct-debit/guarantee/">direct debit guarantee.</a></small>
                     </p>
                 </div>
             </div>
