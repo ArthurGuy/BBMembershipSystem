fb43b8131710ee1aae4aa28044e278e97b6edf8a
Arthur Guy <arthur@assys.net>
2015-03-05 14:14:09 +0000

    method which itterates over log data mering and deleting records

---
+++ app/BB/Services/CombineEquipmentLogs.php
@@ -0,0 +1,76 @@
+<?php namespace BB\Services;
+
+use BB\Repo\EquipmentLogRepository;
+
+class CombineEquipmentLogs {
+
+
+    /**
+     * @var EquipmentLogRepository
+     */
+    private $equipmentLogRepository;
+
+    function __construct(EquipmentLogRepository $equipmentLogRepository)
+    {
+        $this->equipmentLogRepository = $equipmentLogRepository;
+
+        $this->logEntries = $this->equipmentLogRepository->getUnprocessedRecords();
+    }
+
+    public function run()
+    {
+        $updating = true;
+
+        while ($updating) {
+            $updating = $this->updateLogEntries();
+        }
+    }
+
+    private function fetchNextRecord($userId, $device, $reason, \DateTime $finishedDate)
+    {
+        foreach ($this->logEntries as $entry) {
+            if (($entry['user_id'] == $userId) && ($entry['device'] == $device) && ($entry['reason'] == $reason)) {
+                if (($entry['started']->gt($finishedDate)) && ($entry['started']->diffInSeconds($finishedDate) <= 60)) {
+                    return $entry;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    private function updateLogEntries()
+    {
+        foreach ($this->logEntries as $entry) {
+
+            //look all the subsequent records for related entries
+
+            //The following must be the same
+            $entry['user_id'];
+            $entry['device'];
+            $entry['reason'];
+
+            //See if there is a record we can join with
+            $nextRecord = $this->fetchNextRecord(
+                $entry['user_id'],
+                $entry['device'],
+                $entry['reason'],
+                $entry['finished']
+            );
+
+            if ($nextRecord) {
+
+                $this->equipmentLogRepository->update($entry['id'], ['finished' => $nextRecord['finished']]);
+                $this->equipmentLogRepository->delete($nextRecord['id']);
+
+                //The array is now dirty - refetch it and return to restart the check
+                $this->logEntries = $this->equipmentLogRepository->getUnprocessedRecords();
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
