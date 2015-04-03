defbbf260c9576fe6a03fb2ad6afa1b6cc7c1925
Arthur Guy <arthur@assys.net>
2015-04-03 14:48:28 +0100

    Added a reference field to the payment form. This is currently only used by the balance payment method

--- app/views/partials/payment-form.blade.php
+++ app/views/partials/payment-form.blade.php
@@ -1,24 +1,25 @@
 {{ Form::open(['method'=>'POST', 'href' => '', 'class'=>'form-inline js-multiPaymentForm']) }}
-        {{ Form::hidden('reason', $reason) }}
-        {{ Form::hidden('display_reason', $displayReason, ['class'=>'js-paymentDescription']) }}
-        {{ Form::hidden('stripe_token', '', ['class'=>'js-stripeToken']) }}
-        {{ Form::hidden('return_path', $returnPath) }}
+    {{ Form::hidden('reason', $reason) }}
+    {{ Form::hidden('display_reason', $displayReason, ['class'=>'js-paymentDescription']) }}
+    {{ Form::hidden('stripe_token', '', ['class'=>'js-stripeToken']) }}
+    {{ Form::hidden('return_path', $returnPath) }}
+    {{ Form::hidden('ref', isset($ref)?$ref:null) }}
 
-        @if ($amount != null)
-            {{ Form::hidden('amount', $amount, ['class'=>'js-amount']) }}
-        @else
-            <div class="form-group">
-                <div class="input-group">
-                    <div class="input-group-addon">&pound;</div>
-                    {{ Form::input('number', 'amount', '10.00', ['class'=>'form-control js-amount', 'step'=>'0.01', 'required'=>'required']) }}
-                </div>
-            </div>
-        @endif
+    @if ($amount != null)
+        {{ Form::hidden('amount', $amount, ['class'=>'js-amount']) }}
+    @else
         <div class="form-group">
-            {{ HTML::paymentFormMethodDropdown($methods) }}
+            <div class="input-group">
+                <div class="input-group-addon">&pound;</div>
+                {{ Form::input('number', 'amount', '10.00', ['class'=>'form-control js-amount', 'step'=>'0.01', 'required'=>'required']) }}
+            </div>
         </div>
-        {{ Form::submit($buttonLabel, array('class'=>'btn btn-primary')) }}
-        <div class="has-feedback has-error">
-            <span class="help-block"></span>
-        </div>
+    @endif
+    <div class="form-group">
+        {{ HTML::paymentFormMethodDropdown($methods) }}
+    </div>
+    {{ Form::submit($buttonLabel, array('class'=>'btn btn-primary')) }}
+    <div class="has-feedback has-error">
+        <span class="help-block"></span>
+    </div>
 {{ Form::close() }}
