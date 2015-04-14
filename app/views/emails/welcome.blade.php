86ba27fb30e969919718e22f6609e0d8dd1bfa9b
Arthur Guy <arthur@assys.net>
2015-04-14 11:27:15 +0100

    policy and slack links added to the welcome email

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -10,8 +10,8 @@
     Hi {{ $user['given_name'] }},<br />
     Thanks for signing up to be part of Build Brighton, Brighton's amazing maker space.
 </p>
+<h3>Next Steps</h3>
 <p>
-    <h3>Next Steps</h3>
     The first thing to do is to arrange your induction.
     This will be a full overview of Build Brighton from where everything is and what to do if something goes
     wrong through to where the bins get emptied.<br />
@@ -22,12 +22,18 @@
     We would also like you to join the mailing list, this is where most of the communication between members occurs and is where
     things get decided and you find out about what's going on.<br />
     <a href="https://groups.google.com/forum/#!forum/brighton-hackspace-members">Mailing list</a> - please use the same
-    email address as you did when signing up.
+    email address as you did when signing up.<br />
+    <br />
+    We also have an active community on <a href="https://slack.com/">Slack</a>,
+    for an invite please email the <a href="mailto:trustees@buildbrighton.com">trustees</a>
 </p>
 <p>
     We have a code of conduct which we expect all members to abide by and a set of rules which govern how things are run,
     please take a moment and read these.<br />
-    <a href="http://buildbrighton.com/wiki/Rules">Rules</a> and the <a href="http://buildbrighton.com/wiki/Code_of_Conduct">Code of Conduct</a>.<br />
+    <a href="https://bbms.buildbrighton.com/resources/policy/rules">Rules</a>
+    and the <a href="https://bbms.buildbrighton.com/resources/policy/code-of-conduct">Code of Conduct</a>.<br />
+    There are also a number of policies which manage and guide other areas of Build Brighton,
+    these can be found on the <a href="https://bbms.buildbrighton.com/resources">resources</a> page.<br />
     If you ever have any concerns about any aspect of Build Brighton please email the <a href="mailto:trustees@buildbrighton.com">trustees</a>.
 </p>
 <p>
