aae2eb6ae19a5069d0ea5f402d0a6e8a52abf4b6
Arthur Guy <arthur@assys.net>
2015-05-13 14:14:15 +0100

    fixed the notification tests for the updated handling

--- app/tests/codeception/functional/NotificationCept.php
+++ app/tests/codeception/functional/NotificationCept.php
@@ -5,19 +5,19 @@ $I->wantTo('test the notifications display on the page');
 Notification::message('General Message');
 $I->amOnPage('/');
 $I->see('General Message');
-$I->seeElement('.snackBar');
+$I->seeElement('#snackbarMessage');
 
 
 Notification::success('Success Message');
 $I->amOnPage('/');
 $I->see('Success Message');
-$I->seeElement('.snackBar');
+$I->seeElement('#snackbarMessage');
 
 
 Notification::error('Error Message');
 $I->amOnPage('/');
 $I->see('Error Message');
-$I->seeElement('.snackBar');
+$I->seeElement('#snackbarMessage');
 
 
 $details = new \Illuminate\Support\MessageBag(['line1'=>'Error Line 1', 'line2'=>'Error Line 2']);
@@ -26,5 +26,6 @@ $I->amOnPage('/');
 $I->see('Error Message');
 $I->see('Error Line 1');
 $I->see('Error Line 2');
-$I->seeElement('.snackBar');
+$I->seeElement('#snackbarMessage');
+$I->seeElement('#snackbarMessages');
 
