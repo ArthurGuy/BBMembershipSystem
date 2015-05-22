288fcfc73d402eae0819e6188c84321497de3b1a
Arthur Guy <arthur@assys.net>
2015-05-22 10:18:42 +0100

    docblock fix

--- app/BB/Helpers/GoCardlessHelper.php
+++ app/BB/Helpers/GoCardlessHelper.php
@@ -79,10 +79,10 @@ class GoCardlessHelper
 
     /**
      * Create a new payment against a preauth
-     * @param      $preauthId
-     * @param      $amount
-     * @param null $name
-     * @param null $description
+     * @param             $preauthId
+     * @param             $amount
+     * @param null|string $name
+     * @param null|string $description
      * @return bool
      */
     public function newBill($preauthId, $amount, $name = null, $description = null)
