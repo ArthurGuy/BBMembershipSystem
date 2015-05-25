468e4f3cab98f0e9ef52357571987849ad505723
Arthur Guy <arthur@assys.net>
2015-05-25 12:06:37 +0100

    created an expenses repo, filled out the expenses entity and created a presenter. Repo also fires events which are used to generate the payment records

--- app/BB/Entities/Expense.php
+++ app/BB/Entities/Expense.php
@@ -2,6 +2,7 @@
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Laracasts\Presenter\PresentableTrait;
 
 /**
  * Class Expense
@@ -14,10 +15,14 @@ use Illuminate\Database\Eloquent\Model;
  * @property integer $amount
  * @property Carbon  $expense_date
  * @property string  $file
+ * @property bool    $approved
+ * @property bool    $declined
+ * @property integer $approved_by_user
  * @package BB\Entities
  */
 class Expense extends Model
 {
+    use PresentableTrait;
 
     /**
      * The database table used by the model.
@@ -41,10 +46,21 @@ class Expense extends Model
         'file',
     ];
 
+    protected $presenter = 'BB\Presenters\ExpensePresenter';
+
+    public function getDates()
+    {
+        return array('created_at', 'updated_at', 'expense_date');
+    }
 
     public function user()
     {
         return $this->belongsTo('\BB\Entities\User');
+    }
+
+    public function approvedBy()
+    {
+        return $this->belongsTo('\BB\Entities\User', 'approved_by_user');
     }
 
 
---
+++ app/BB/Presenters/ExpensePresenter.php
@@ -0,0 +1,33 @@
+<?php namespace BB\Presenters;
+
+use Laracasts\Presenter\Presenter;
+
+class ExpensePresenter extends Presenter
+{
+
+    public function category()
+    {
+        switch ($this->entity->category) {
+            case 'consumables':
+                return 'Consumables';
+            default:
+                return $this->entity->category;
+        }
+    }
+
+    public function expense_date()
+    {
+        return $this->entity->expense_date->toFormattedDateString();
+    }
+
+    public function amount()
+    {
+        return '&pound;' . number_format($this->entity->amount / 100, 2);
+    }
+
+    public function file()
+    {
+        return 'https://s3-eu-west-1.amazonaws.com/' . $_ENV['S3_BUCKET'] . '/' . $this->entity->file;
+    }
+
+} 
