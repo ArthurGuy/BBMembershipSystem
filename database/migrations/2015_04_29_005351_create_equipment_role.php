14aaa8f1810c1f4187bca9e73fc0219ec19ed5df
Arthur Guy <arthur@assys.net>
2015-06-04 00:55:47 +0100

    updated a migration to let it work with unit tests

--- database/migrations/2015_04_29_005351_create_equipment_role.php
+++ database/migrations/2015_04_29_005351_create_equipment_role.php
@@ -1,5 +1,6 @@
 <?php
 
+use BB\Entities\Role;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -12,7 +13,7 @@ class CreateEquipmentRole extends Migration {
 	 */
 	public function up()
 	{
-        DB::table('roles')->insert(['name'=>'equipment', 'title'=>'Manage Equipment', 'created_at'=>new DateTime()]);
+        Role::create(['name'=>'equipment', 'title'=>'Manage Equipment']);
 	}
 
 	/**
@@ -22,7 +23,7 @@ class CreateEquipmentRole extends Migration {
 	 */
 	public function down()
 	{
-        DB::table('roles')->where('name', '=', 'equipment')->delete();
+        Role::where('name', 'equipment')->delete();
 	}
 
 }
