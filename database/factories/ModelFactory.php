e3b25e7eefb8fff1fd15ca4da83f8c2e40381bb6
Arthur Guy <arthur@assys.net>
2015-06-04 13:59:26 +0100

    nulll edit

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,27 +12,27 @@
 */
 $factory->define('BB\Entities\User', function ($faker) {
     return [
-        'given_name' => $faker->firstName,
-        'family_name' => $faker->lastName,
-        'email' => $faker->email,
-        'password' => str_random(10),
-        'remember_token' => str_random(10),
-        'hash' => str_random(32),
-        'status' => 'active',
+        'given_name'          => $faker->firstName,
+        'family_name'         => $faker->lastName,
+        'email'               => $faker->email,
+        'password'            => str_random(10),
+        'remember_token'      => str_random(10),
+        'hash'                => str_random(32),
+        'status'              => 'active',
         'induction_completed' => false,
-        'trusted' => false,
-        'key_holder' => false,
-        'phone' => false,
+        'trusted'             => false,
+        'key_holder'          => false,
+        'phone'               => false,
     ];
 });
 
 $factory->define('BB\Entities\ProfileData', function ($faker) {
     return [
-        'user_id' => null,
-        'profile_photo' => false,
-        'new_profile_photo' => false,
+        'user_id'               => null,
+        'profile_photo'         => false,
+        'new_profile_photo'     => false,
         'profile_photo_private' => false,
         'profile_photo_on_wall' => false,
-        'tagline' => $faker->sentence
+        'tagline'               => $faker->sentence
     ];
 });
