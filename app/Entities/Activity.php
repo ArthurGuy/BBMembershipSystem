4945e7c5e99694373842e1ef231aab2fdcb25295
Arthur Guy <arthur@assys.net>
2015-06-08 19:49:16 +0100

    improved docblock

--- app/Entities/Activity.php
+++ app/Entities/Activity.php
@@ -1,15 +1,18 @@
 <?php namespace BB\Entities;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Activity
  *
+ * @property integer $id
  * @property integer $key_fob_id
  * @property integer $user_id
  * @property string $service
  * @property string $response
  * @property bool $delayed
+ * @property Carbon $created_at
  * @package BB\Entities
  */
 class Activity extends Model
