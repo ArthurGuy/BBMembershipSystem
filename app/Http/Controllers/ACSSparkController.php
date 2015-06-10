25498bfe48dfe9a94299d0a02d27a9aaac2e0a7e
Arthur Guy <arthur@assys.net>
2015-06-10 09:03:44 +0100

    create a charge when the users tag is scanned

--- app/Http/Controllers/ACSSparkController.php
+++ app/Http/Controllers/ACSSparkController.php
@@ -1,6 +1,7 @@
 <?php namespace BB\Http\Controllers;
 
 use BB\Repo\DeviceRepository;
+use BB\Repo\PaymentRepository;
 use BB\Services\KeyFobAccess;
 use BB\Validators\ACSValidator;
 
@@ -12,19 +13,19 @@ class ACSSparkController extends Controller
      */
     private $deviceRepository;
     /**
-     * @var ACSValidator
-     */
-    private $ACSValidator;
-    /**
      * @var KeyFobAccess
      */
     private $keyFobAccess;
+    /**
+     * @var PaymentRepository
+     */
+    private $paymentRepository;
 
-    function __construct(DeviceRepository $deviceRepository, ACSValidator $ACSValidator, KeyFobAccess $keyFobAccess)
+    function __construct(DeviceRepository $deviceRepository, KeyFobAccess $keyFobAccess, PaymentRepository $paymentRepository)
     {
         $this->deviceRepository = $deviceRepository;
-        $this->ACSValidator     = $ACSValidator;
         $this->keyFobAccess     = $keyFobAccess;
+        $this->paymentRepository = $paymentRepository;
     }
 
     public function handle()
@@ -38,6 +39,8 @@ class ACSSparkController extends Controller
             return \Response::make('Not found', 404);
         }
         $user = $keyFob->user()->first();
+
+        $this->paymentRepository->recordPayment($data['service'], $user->id, 'balance', null, 0.05, 'paid', 0, $data['device']);
 
         return \Response::make('OK', 201);
     }
