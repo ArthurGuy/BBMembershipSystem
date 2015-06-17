9053a3519509af26b845dbc913291a40b1ead05e
Arthur Guy <arthur@assys.net>
2015-06-17 19:02:57 +0100

    new acs service - sensor

--- app/Validators/ACSValidator.php
+++ app/Validators/ACSValidator.php
@@ -5,7 +5,7 @@ class ACSValidator extends FormValidator
 
     protected $rules = [
         'device'  => 'required|max:25|exists:devices,device_id',
-        'service' => 'required|in:entry,usage,consumable,shop,status,device-scanner',
+        'service' => 'required|in:entry,usage,consumable,shop,status,device-scanner,sensor',
         'message' => 'required|in:boot,heartbeat,lookup,start,stop,charge,error,update',
         'tag'     => 'max:15',
         'time'    => 'max:10|integer',
