7cba3a578f1a7e0e58edf6d72cbe4aa3b5dc034f
Arthur Guy <arthur@assys.net>
2015-06-26 10:32:01 +0100

    welcome email verify link fixed and improved some of the copy

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -13,8 +13,6 @@
 <h3>Next Steps</h3>
 <p>
     The first thing to do is to arrange your induction.
-    This will be a full overview of Build Brighton from where everything is and what to do if something goes
-    wrong through to where the bins get emptied.<br />
     The process for arranging this is still being sorted out so for now please email us at
     <a href="mailto:info@buildbrighton.com">info@buildbrighton.com</a> and let us know if you're going to be coming
     to the space on Thursday (our open evening) and we will make sure someone will be around to show you the ropes.<br />
@@ -29,16 +27,19 @@
 </p>
 <p>
     We have a code of conduct which we expect all members to abide by and a set of rules which govern how things are run,
-    please take a moment and read these.<br />
+    please make sure you read and accept these.<br />
+    If you have any questions please email the <a href="mailto:trustees@buildbrighton.com">trustees</a><br />
     <a href="https://bbms.buildbrighton.com/resources/policy/rules">Rules</a>
     and the <a href="https://bbms.buildbrighton.com/resources/policy/code-of-conduct">Code of Conduct</a>.<br />
+    <br />
     There are also a number of policies which manage and guide other areas of Build Brighton,
-    these can be found on the <a href="https://bbms.buildbrighton.com/resources">resources</a> page.<br />
+    these can be found on the <a href="https://bbms.buildbrighton.com/resources">resources</a> page and we recommend you take a look at them.<br />
+    <br />
     If you ever have any concerns about any aspect of Build Brighton please email the <a href="mailto:trustees@buildbrighton.com">trustees</a>.
 </p>
 <p>
     Finally, if you could follow the link below to confirm your email address that would help us ensure we have a accurate details.<br />
-    {!! URL::route('account.confirm-email', [$user['id'], $user['hash']]) !!}.<br/>
+    <a href="{!! URL::route('account.confirm-email', [$user['id'], $user['hash']]) !!}">{!! URL::route('account.confirm-email', [$user['id'], $user['hash']]) !!}</a>.<br/>
 </p>
 <p>
     Thank you,
