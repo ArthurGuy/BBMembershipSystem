c40d84562dd8f60deb6a7e16d65a212b10bb87e0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-14 10:29:48 +0800

    Delete resources directory

--- resources/views/detected_devices/index-row.blade.php
+++
@@ -1,7 +0,0 @@
-<tr>
-    <td>{{ $device->created_at }}</td>
-    <td>{{ $device->type }}</td>
-    <td>{{ $device->mac_address }}</td>
-    <td>{{ $device->display_name }}</td>
-    <td>{{ $device->occurrences }}</td>
-</tr>
--- resources/views/partials/js-data.blade.php
+++
@@ -1,7 +0,0 @@
-<input type="hidden" id="stripePublicKey" value="{{ $stripeKey }}" />
-@if (!Auth::guest())
-    <input type="hidden" id="memberEmail" value="{{ Auth::user()->email }}" />
-    <input type="hidden" id="userId" value="{{ Auth::user()->id }}" />
-@endif
-
-<input type="hidden" id="csrfToken" value="<?php echo csrf_token(); ?>">
