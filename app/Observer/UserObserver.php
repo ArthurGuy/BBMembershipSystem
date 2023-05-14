f086c402e882ff70e456df00c51352099956a87c
Neil Stainton <nes0@eml.cc>
2023-05-14 10:58:14 +0100

    Post UserObserver notifications to a Discord Webhook

--- app/Observer/UserObserver.php
+++ app/Observer/UserObserver.php
@@ -1,6 +1,7 @@
 <?php namespace BB\Observer;
 
 use BB\Mailer\UserMailer;
+use GuzzleHttp\Client as Guzzle;
 
 class UserObserver
 {
@@ -79,5 +80,13 @@ class UserObserver
         // if (\App::environment('production')) {
         //     \Slack::to($channel)->send($message);
         // }
+
+        $client = new Guzzle();
+        $url = env('DISCORD_WEBHOOK', '');
+        if ($url) {
+            $client->post($url, [
+                GuzzleHttp\RequestOptions::JSON => ['content' => $message]
+            ]);
+        }
     }
-} 
+}
