33a438f0e9271eb02c1e0b2041a80b3d7d571f8a
Neil Stainton <nes0@eml.cc>
2024-03-30 11:05:11 +0000

    Policies: new rules, code of conduct and welcome letter

--- resources/polices/code-of-conduct.md
+++ resources/polices/code-of-conduct.md
@@ -1,16 +1,98 @@
-#Code Of Conduct
+# Code Of Conduct
 
-Build Brighton is an inclusive community where all our members and visitors should feel welcome.
-This code applies to everyone's conduct on all public and private communication channels (such as IRC and mailing lists), as well as at the makerspace itself.
+## Introduction
 
-This code of conduct is in addition to the Rules which cover safety and the ethos of the space.
+The Build Brighton trustees are dedicated to providing a friendly, safe, and
+welcoming environment for all. We invite all those who participate in our
+community to help us create safe and positive experiences for everyone. This
+code applies to everyone´s conduct whilst in the space or participating in
+related public and private communication channels such as Discord.
 
-* It is unacceptable to harass or discriminate against others for any reason.
-* Keep all communication civil, non-aggressive and keep swearing to a minimum; if you can't make a comment politely and persuasively, don't make it at all. Your conduct reflects on the image of Build Brighton as seen by outsiders.
-* Public (http://www.catb.org/jargon/html/T/troll.html) trolling is never appropriate.
-* Personal attacks will not be tolerated.
+This code of conduct is in addition to the Rules which cover safety and the
+ethos of the space.
 
-If you ignore these guidelines you will initially be warned or moderated. Subsequent offences may result in you being banned from the mailing list or IRC. Additionally, the trustees may start proceedings under the Grievance Procedure.
+## Behaviour in the Space
 
-If you witness any persistent breaches of this code, or have any other concerns, please contact the trustees at trustees@buildbrighton.com.
-The trustees are powerless to fix problems they aren't aware of.
+1. **Respect:** Treat everyone with respect. We are a diverse community of
+individuals with different backgrounds, experiences, and perspectives.
+2. **Safety:** Prioritize safety. Always use equipment properly and follow
+safety guidelines.
+3. **Cleanliness:** Keep the space clean. Clean up after yourself and respect
+the shared nature of the space.
+
+Please be civil and respectful to other members and their work. Be friendly to
+visitors. Generally speaking, if you can't find something positive to say, then
+best to not comment at all.
+
+Discriminatory language, harassment, threatening behaviour or aggression is
+absolutely not acceptable. If you witness it in the space, then please raise it
+with the trustees.
+
+Don't disturb anyone working at machinery or clearly concentrating on their
+work.
+
+Do speak up if you see dangerous activity, either directly or raise it with the
+trustees. If another member raises a safety concern with you, this should not
+be construed as harassment. Engage in constructive dialogue and try to reach an
+shared understanding. Do reach out to other members or feel free to raise an
+issue with the trustees if in any doubt about working safely.
+
+Do help keep the space clean. It doesn't tidy itself and we do not employ
+cleaners. It's a great feeling knowing you have left the area a little tidier
+than you found it.
+
+## Behaviour on Discord and Any Other Forums or Media
+
+1. **Positive Communication:** Use respectful language and be considerate of
+others' viewpoints. Avoid offensive or derogatory comments.
+2. **Privacy:** Respect the privacy of others. Do not share personal information
+without consent.
+3. **No Spam:** Avoid spamming or posting irrelevant content.
+
+As in real life, keep posts civil and respectful. We have zero tolerance for
+discriminatory language.
+
+Always be tactful in your comments and don't make anyone feel bad about their
+work. We are a community of amazing makers and the forums are a great place to
+build confidence through mutual support. If you can't be positive, don't
+comment!
+
+Online harassment or trolling is not acceptable behaviour. Don't do it and if
+you are on the receiving end, report it to the trustees to deal with.
+
+## Grievance Procedure
+
+If you experience or witness unacceptable behaviour, please raise it with the
+trustees as soon as possible. All reports will be handled with discretion and
+confidentiality.
+
+If a conflict cannot be resolved through mediation, the very last resort is a
+tribunal led by the trustees. The collective or majority decision of the
+trustees is final.
+
+## Administration of this Code
+
+The trustees are responsible for clarifying and enforcing our standards of
+acceptable behaviour and will take appropriate and fair corrective action in
+response to any behaviour that they deem inappropriate, threatening, offensive,
+or harmful.
+
+If you are held to have violated these guidelines by the trustees, you will
+normally be warned and may be asked to apologise. However serious violations
+may result in an immediate sanction. Repeated violations may result in removal
+of trusted status and unsupervised access, or removal from the membership. If a
+member is sanctioned, we do not refund tool payments or other fees.
+
+If you witness any persistent breaches of this code, or have any other concerns,
+please contact the trustees on Discord or email trustees@buildbrighton.com. The
+trustees are powerless to fix problems they are not aware of.
+
+## Conclusion
+
+By participating in our community, you agree to abide by this code of conduct,
+and cooperate with the trustees who enforce it. Let's work together to make our
+maker space a welcoming and friendly environment for all!
+
+---
+
+_Updated March 2024_
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,46 +1,122 @@
 <!DOCTYPE html>
 <html lang="en-GB">
 <head>
-    <meta charset="utf-8">
+  <meta charset="utf-8">
+  <title></title>
 </head>
 <body>
-<h1>Welcome to Build Brighton</h1>
-
-<p>
-    Hi {{ $user['given_name'] }},<br />
-    Thanks for joining the BuildBrighton maker space
-</p>
-<h3>Next Steps</h3>
-<p>
-    <b>1. Join Discord say hello and Arrange an Induction</b><br /><br />
-    The first thing is to Discord - a collaborative network for group and individual messaging.<br /><br />
-    For an invite to <a href="https://discord.gg">Discord</a> please install the client on desktop or mobile, and click <a href="https://discord.gg/tzgfE2Q5qh">this link</a><br /><br />
-    Once you’re signed in, please update your nickname to your 'real name' by right clicking yourself in sidebar and selecting "edit server profile". You can join any other channels that take your interest such as 3D printing, electronics and photography.
-</p>
-<p>
-    <b>2. Read the Code of Conduct and Rules</b><br />
-    We have a <a href="https://bbms.buildbrighton.com/resources/policy/code-of-conduct">Code of Conduct</a> which we expect all members to follow and a set of <a href="https://bbms.buildbrighton.com/resources/policy/rules">Rules</a> which govern how things are run. Please make sure you read and accept these.<br />
-    The main points are: Be safe, keep things tidy and make a contribution. We’re a members club and the place gets better when members make it better. So if you spot something that could be improved we encourage a pro-active attitude, in collaboration with other members. All roles are done by volunteers, including the trustees.<br />
-    If you break something or find something is not working, please let the members know (e.g. via Discord).<br /><br />
-    Our policies are accessible from the <a href="https://bbms.buildbrighton.com/resources">resources</a> page of the Build Brighton Members System (BBMS).
-</p>
-<p>
-    <b>3. Storage</b><br /><br />
-    There is limited storage space for members, <a href="https://bbms.buildbrighton.com/storage_boxes"> Here is where you can Rent one </a> but larger items can be kept in the space for a short time if agreed beforehand. To reserve your storage box go to the Members Storage page in BBMS. Each member can claim up to 19L of member storage, this can be one 19L box or a combination of 4L and 9L boxes. Each individual box requires a £5 payment.
-</p>
-<p>
-    <b>4. Any questions and concerns</b><br /><br />
-    If you have any questions please use Discord where a member who may be able to help, or point you in the direction of someone who can. If you need to contact the trustees please email us at <a href="mailto:trustees@buildbrighton.com">trustees@buildbrighton.com</a>. 
-</p>
-<p>
-    <b>5. Confirm your email address</b><br /><br />
-    Finally, please click the link below to confirm your email address and ensure we have your accurate details.<br /><br />
-    <a href="{!! URL::route('account.confirm-email', [$user['id'], $user['hash']]) !!}">{!! URL::route('account.confirm-email', [$user['id'], $user['hash']]) !!}</a>
-</p>    
-<p>
-    Thank you and we hope you enjoy being a member of Build Brighton. <br /><br />
-    Best wishes, <br />
-    The Build Brighton Trustees
-</p>
+  <h1>Welcome to Build Brighton</h1>
+  <p>Hi {{ $user['given_name'] }},<br>
+  <br>
+  Thanks for joining the Build Brighton maker space</p>
+  <h2>Essential!</h2>
+  <h3>Confirm your email address</h3><br>
+  Please click the link below to confirm your email address to ensure we have
+  your accurate details.<br>
+  <br>
+  <a href=
+  "{!!%20URL::route('account.confirm-email',%20[$user['id'],%20$user['hash']])%20!!}">
+  {!! URL::route('account.confirm-email', [$user['id'], $user['hash']]) !!}</a>
+  <h3>Your membership is not activated until this step is complete!</h3>
+  <h3>Next Steps</h3>
+  <p><b>1. Join Discord say hello and arrange an induction</b><br>
+  <br>
+  The first thing is to join Discord - a collaborative network for group and
+  individual messaging. This has information on the current state of things and
+  we use it to chat between members.<br>
+  <br>
+  For an invite to <a href="https://discord.gg">Discord</a> please install the
+  client on desktop or mobile, and click <a href=
+  "https://discord.gg/tzgfE2Q5qh">this link</a><br>
+  <br>
+  Once you are signed in, please update your nickname to your 'real name' by
+  right clicking yourself in sidebar and selecting "edit server profile". This
+  is so that the trustees can identify you as a paid-up member on Discord.<br>
+  You will then need to wait for a trustee to update your profile. (The default
+  profile is restricted as anyone with a link can join our Discord server.)
+  Until your profile is updated, you can only post in the #welcome channel. If
+  the update does not happen, see (7) below.<br>
+  Once your profile is updated, we will let you know and you can join other
+  channels of interest, and you can request equipment inductions in the
+  #inductions channel.</p>
+  <p><b>2. Read the Rules and Code of Conduct</b><br>
+  We have <a href="https://bbms.buildbrighton.com/resources/policy/rules">some
+  rules</a> which we expect all members to follow. These govern how things are
+  run. Please make sure you read these.<br>
+  It is a condition of membership that you adhere to the rules and call out
+  members who are breaking them.<br>
+  The main points are: Be safe, keep things tidy and contribute to maintaining
+  a positive and happy environment for everyone.</p>
+  <p>Please read and familiarise youself with our <a href=
+  "https://bbms.buildbrighton.com/resources/policy/code-of-conduct">code of
+  conduct</a>. Behave well towards other members and visitors. Do not do things
+  or post messages that will offend and do not harass anyone in our space.</p>
+  <p>We are a members club and the place gets better when members make it
+  better. So if you spot something that could be improved we encourage you to
+  be proactive and collaborate with other members. All the administration of
+  the space from the cleaning to maintaining the infrastructure to being a
+  trustee is done by volunteers.<br>
+  If you break something or find something is not working, please let the
+  members know (e.g. via Discord).<br>
+  <br>
+  Our Rules and policies are accessible (once you have logged in - see 8 below)
+  from the <a href="https://bbms.buildbrighton.com/resources">resources</a>
+  page of the Management System.</p>
+  <p><b>3. Storage</b><br>
+  <br>
+  You can, on payment of a £15 non-refundable fee, obtain one 19 litre storage
+  box for your use. To do this, go to the <a href=
+  "https://bbms.buildbrighton.com/storage_boxes">Members Storage page</a> in
+  our management system (see 8 below).<br>
+  There are also storage lockers with a monthly rental depending on size - for
+  availability see the relevant channel on our Discord server.<br>
+  There is limited storage space for members in our three rooms. Large items
+  can be left in the space for a week or two (depending on the their size,
+  larger things should be left for shorter times) if agreed beforehand. Items
+  left in the space must have an do not hack sign with your name and a removal
+  date.</p>
+  <p><b>4. Any questions and concerns</b><br>
+  <br>
+  If you have any questions please use discord where a member who may be able
+  to help, or point you in the direction of someone who can. If you need to
+  contact the trustees please see 7 below.</p>
+  <p><b>5.Visiting the Space</b><br></p>
+  <p>The space is often unoccupied. If you plan to visit please use discord to
+  engage with members and to see who will be in the space.<br>
+  The activity channel on Discord shows when members enter the space but they
+  can depart at any time.<br>
+  Do not just turn up at the space and expect someone to let you in! Until you
+  become a keyholder, arrange to meet someone there before setting out.</p>
+  <p><b>6. Refunds and cancellation</b><br>
+  <br>
+  We will not refund membership fees even if you are not using the space. To
+  cancel your subscription, go to your member page, and then scroll down to the
+  bottom after "Submit a new expense", then press the link 'Cancel your direct
+  debit and leave'. Check the payment has been cancelled. If you subscribed
+  using Paypal then cancel via Paypal.</p>
+  <p><b>7. Contacting the trustees</b><br>
+  <br>
+  If you need to contact the trustees please email us at <a href=
+  "mailto:trustees@buildbrighton.com">trustees@buildbrighton.com</a>.<br>
+  If you do need to contact us, bear in mind that we are not magicians, we may
+  have jobs and families which are our main priority.<br>
+  We cannot necessarily fix things or rectify problems easily and we may not do
+  things exactly the same way given similar circumstances, though we try to be
+  consistent and fair as far as possible.</p>
+  <p><b>8. Our Management System</b><br>
+  <br>
+  As a member, please explore our <a href=
+  "https://bbms.buildbrighton.com/">management system</a>. Log in using the
+  email address that you entered on the joining form. This allows you to do
+  things such as check the status of our equipment, submit expenses and manage
+  payments for use of tools or storage.</p>
+  <p><b>9. Finally, Thank you and we hope you enjoy being a member of Build
+  Brighton.</b><br>
+  <br></p>
+  <p>Best wishes,<br>
+  <br>
+  The Build Brighton Trustees</p>
+  <hr>
+  <p>updated March 2024</p>
 </body>
 </html>
